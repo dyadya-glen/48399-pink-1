08b1a28a55b74048b096458f59e0e19129f27917
dyadya-glen <glen2001@yandex.ru>
2016-04-06 04:22:40 +0300

    Попытка отчаянья 4

--- js/main.js
+++ js/main.js
@@ -21,20 +21,6 @@ clased.addEventListener("click", function (event) {
   clased.classList.add("main-nav__clased--show");
 });
 
-// for (var i = 0; i < control.length; i++) {
-//   control[i].addEventListener("click", function (event) {
-//     event.preventDefault();
-//     for (var j = 0; j < review.length; j++) {
-//       if (review[j].classList.contains("review--show")) {
-//         review[j].classList.remove("review--show");
-//       }
-//       else {
-//         review[j++].classList.add("review--show");
-//       }
-//     }
-//   });
-// }
-
 ///////////////////////КАРТА/////////////////////////////
 function initialize() {
   var mapOptions = {
@@ -43,7 +29,7 @@ function initialize() {
   }
   var map = new google.maps.Map(document.getElementById('map-canvas'),
                                 mapOptions);
-  var image = "../img/icons/icon../img/icons/icon-map-marker.svg";
+  var image = "img/icons/icon-map-marker.svg";
   var myLatLng = new google.maps.LatLng(59.936128,30.3210702);
   var beachMarker = new google.maps.Marker({
     position: myLatLng,
